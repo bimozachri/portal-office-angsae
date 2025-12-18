4410628672f1fe9bc45c7810a1fa28d7d7e8d06f
Bimo Zachriansyah Wicaksono Hermawan <126762824+bimozachri@users.noreply.github.com>
2025-12-18 08:47:31 +0700

    Add files via upload

---
+++ .net/static/chunks/app_page_tsx_8209cd1b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7f0e62b9._.js",
+  "static/chunks/_998bc4a5._.js"
+],
+    source: "dynamic"
+});
