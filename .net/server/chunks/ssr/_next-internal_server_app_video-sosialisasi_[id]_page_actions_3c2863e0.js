32c56a0b48a5daf0ca418fbc115f8600b341d0b4
Bimo Zachriansyah Wicaksono Hermawan <126762824+bimozachri@users.noreply.github.com>
2025-12-18 08:45:52 +0700

    Add files via upload

---
+++ .net/server/chunks/ssr/_next-internal_server_app_video-sosialisasi_[id]_page_actions_3c2863e0.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/video-sosialisasi/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_video-sosialisasi_%5Bid%5D_page_actions_3c2863e0.js.map
---
+++ .net/server/chunks/ssr/_next-internal_server_app_video-sosialisasi_page_actions_beea00bc.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/video-sosialisasi/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_video-sosialisasi_page_actions_beea00bc.js.map
